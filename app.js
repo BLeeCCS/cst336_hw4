0b95dad82984c120c717f7d8817d1b88ff2283a7
Brandon Lee <bleeccs@gmail.com>
2020-11-25 05:07:51 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.render("index.html"); 
+});
+
+app.get("/home", function(req, res){
+    res.render("index.html"); 
+});
+
+app.get("/superComputer", function(req, res){
+    res.render("superComputer.html"); 
+});
+
+app.get("/mainframeComputer", function(req, res){
+    res.render("mainframeComputer.html"); 
+});
+
+app.get("/personalComputer", function(req, res){
+    res.render("personalComputer.html"); 
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Express server is running...");
+});
